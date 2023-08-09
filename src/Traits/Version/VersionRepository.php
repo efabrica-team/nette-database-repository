b35a472e389a692bebf12962ebaa8ef35ac9bb31
Richard Popelis <popelis@efabrica.sk>
2023-08-09 16:00:06 +0200

    fix VersionRepository

--- src/Traits/Version/VersionRepository.php
+++ src/Traits/Version/VersionRepository.php
@@ -4,15 +4,20 @@ namespace Efabrica\NetteRepository\Traits\Version;
 
 use Efabrica\NetteRepository\Repository\Query;
 use Efabrica\NetteRepository\Repository\Repository;
+use Efabrica\NetteRepository\Repository\RepositoryBehaviors;
 use Efabrica\NetteRepository\Repository\RepositoryDependencies;
 
 /**
  * @extends Repository<Version,Query<Version>>
  */
-abstract class VersionRepository extends Repository
+class VersionRepository extends Repository
 {
     public function __construct(RepositoryDependencies $deps, string $tableName = 'versions')
     {
         parent::__construct($tableName, Version::class, Query::class, $deps);
+    }
+
+    protected function setup(RepositoryBehaviors $behaviors): void
+    {
     }
 }
